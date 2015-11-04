92dea4ae267f20d3dcd69de35d3942c49a9f25e0
Japheth Balane <japheth162@gmail.com>
2015-11-05 00:03:47 +0800

    Project Sphere gameplay constructed

--- script.js
+++ script.js
@@ -10,8 +10,26 @@ var character = new Character();
 var score = 0;
 var points = [];
 var inteval = 20;
+var start = false;
+var xTest = 0;
 
 generatePoints();
+
+var mousePress = function(event) {
+    character.jump = true;
+    start = true;
+    if (event.pageX < character.x) {
+    	xTest = randomBetween(1,5);
+    };
+    if (event.pageX > character.x) {
+    	xTest = randomBetween(-5,-1);
+    };
+    if (event.pageX == character.x) {
+    	xTest = 0;
+    };
+    score++;
+}
+canvas.addEventListener("click", mousePress);
 
 function generatePoints() {
 	for (var i = 5; i < canvas.height; i += inteval) {
@@ -32,20 +50,27 @@ function clearCanvas() {
 
 function world() {
 	clearCanvas();
+	if (start) {
+		character.update();
+	};
 	for (var i = 0; i < points.length; i++) {
 		points[i].update().draw();
 	}
-	character.update();
+	// if (score > 0) {
+	// 	points[score-1].radius = 3;
+	// };
+	console.log(score);
 }
 
 function Point(x,y) {
 	this.x = x;
 	this.y = y;
+	this.radius = 1;
 
 	this.update = function() {
 		this.radius = 1;
 		if (Math.sqrt( Math.abs(this.x-character.x)*Math.abs(this.x-character.x) + 
-			Math.abs(this.y-character.y)*Math.abs(this.y-character.y)) < character.rad) {
+					   Math.abs(this.y-character.y)*Math.abs(this.y-character.y)) < character.rad) {
 			this.radius = character.rad - Math.sqrt( 
 				Math.abs(this.x-character.x)*Math.abs(this.x-character.x) + 
 				Math.abs(this.y-character.y)*Math.abs(this.y-character.y));
@@ -56,7 +81,7 @@ function Point(x,y) {
 
 	this.draw = function() {
 		context.beginPath();
-		context.arc(this.x, this.y, 1, Math.PI*2, false);
+		context.arc(this.x, this.y, this.radius, Math.PI*2, false);
 		context.fillStyle = "rgba(255,255,255,0.15)";
 		context.fill();
 
@@ -68,10 +93,35 @@ function Character() {
 	this.x = canvas.width/2;
 	this.y = canvas.height/2;
 	this.rad = 75;
+	this.jump = false;
+	this.speed = 30;
+	this.spd = this.speed;
+	this.acceleration = 1.01;
 
 	this.update = function() {
+		if (this.y+this.rad < canvas.height && this.y-this.rad > 0 && 
+			this.x+this.rad < canvas.width && this.x-this.rad > 0) {
+			this.speed *= this.acceleration;
+			this.y += this.speed;
+			this.x += xTest;
 
-
-		return this;
+			if (this.jump) {
+				if (this.speed > 0) {
+					this.acceleration = 0.95;
+					this.speed *= -1;
+				};
+				if (this.speed < 0) {
+					this.speed = -this.spd;
+				};
+			};
+			if (this.speed > -0.3 && this.speed < 0) {
+				this.speed = 0;
+				this.acceleration = 1.01;
+			};
+			if (this.speed < this.spd) {
+				this.speed += this.spd / 10;
+			};
+			this.jump = false;
+		};
 	}
 }
