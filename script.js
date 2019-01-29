703293c12a401b1dd71c912eeb6e5479318b2867
Japheth Balane <japheth162@gmail.com>
2019-01-29 18:00:04 +0800

    Rset graphics for initial swarm behaviour improvments

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 var canvas = document.getElementById('swarm');
 var context = canvas.getContext('2d');
-setInterval(world, 30);
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
@@ -10,250 +9,136 @@ canvas.width = window.innerWidth;
 ///////////////////////////////////////////////////////////////
 
 var particles = [];
-var obstacles = [];
 
 ///////////////////////////////////////////////////////////////
 
 function generateParticles(count) {
-	for (var i = 0; i < count; i++) {
-		particles.push(new Particle(randomBetween(0, canvas.width-0), randomBetween(0, canvas.height-0)));
-	}
-}; generateParticles(1000);
-
-function generateObs(count) {
-	for (var i = 0; i < count; i++) {
-		obstacles.push(new Obstacle(randomBetween(0, canvas.width-0), randomBetween(0, canvas.height-0)));
-	}
-}; generateObs(200);
+    for (let i = 0; i < count; i++) {
+        particles.push(new Particle(randomBetween(0, canvas.width-0), randomBetween(0, canvas.height-0)));
+    }
+}; generateParticles(100);
 
 function randomBetween(min, max) {
-	if (min > max) {
-		var temp = min;
-		min = max; max = temp;
-	}
+    if (min > max) {
+        var temp = min;
+        min = max; max = temp;
+    }
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function clearCanvas() {
-	context.fillStyle = "rgba(0,0,0,0.5)";
-	context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = 'rgba(255,255,255,0.01)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function getHypothenuse(p1, p2) {
-	var x = Math.abs(p1.x - p2.x);
-	var y = Math.abs(p1.y - p2.y);
-	return Math.sqrt((x * x) + (y * y));
+    var x = Math.abs(p1.x - p2.x);
+    var y = Math.abs(p1.y - p2.y);
+    return Math.sqrt((x * x) + (y * y));
 }
 
 ///////////////////////////////////////////////////////////////
 
+setInterval(world, 30);
 function world() {
-	clearCanvas();
-	for (var particle of particles) {
-		particle.update().draw();
-	}
-	// for (var o of obstacles) {
-	// 	o.update().draw();
-	// }
+    clearCanvas();
+    for (let particle of particles) {
+        particle.update().draw();
+    }
 }
 
 ///////////////////////////////////////////////////////////////
 
 function Particle(x, y) {
-	this.x = x;
-	this.y = y;
-	this.radius = 3;
-	this.speed = 0.1;
-	this.initialSpeed = this.speed;
-	this.angle = randomBetween(0, 360);
-	this.sensingRadius = 10;
-	this.sensedParticles = [];
-	this.isSwingRight = randomBetween(0,2) == 1 ? true : false;
-	this.steer = randomBetween(0, 2) == 1 ? 0 : 1;
-	this.up = false;
-	this.down = false;
-	this.left = false;
-	this.right = false;
-	this.r = randomBetween(50,255);
-	this.g = randomBetween(50,255);
-	this.b = randomBetween(255,255);
-	this.setMinMaxAngs = function() {
-		this.minAngle = this.angle - 50;
-		this.maxAngle = this.angle + 50;
-	}; this.setMinMaxAngs();
-	this.move = function() {
-		var dx = Math.cos(this.angle * (Math.PI / 180)) * this.speed;
-        var dy = Math.sin(this.angle * (Math.PI / 180)) * this.speed;
+    this.x = x;
+    this.y = y;
+    this.speed = randomBetween(1,3);
+    this.innerRadius = 30;
+    this.outerRadius = 120;
+    this.angle = randomBetween(0,359);
+    this.toAngle = this.angle;
+    this.rotationSpeed = randomBetween(1,5);
+    this.wiggleRange = 30;
+    this.wiggle = randomBetween(-this.wiggleRange,this.wiggleRange);
+    this.wiggleDir = randomBetween(0,2) == 0 ? -1 : 1;
+
+    this.alignment = function() {
+        let sum = this.angle;
+        let ctr = 1;
+        for (let particle of particles) {
+            if (particle != this && getHypothenuse(this, particle) <= this.outerRadius) {
+                sum += particle.angle;
+                ctr++;
+            }
+        }
+        this.angle = this.angle % 360;
+        this.toAngle = (sum / ctr) % 360;
+        if (this.toAngle > this.angle) {
+            this.angle += this.rotationSpeed;
+        } else if (this.toAngle < this.angle) {
+            this.angle -= this.rotationSpeed;
+        }
+    }
+
+    this.updateWiggle = function() {
+        this.wiggle += this.wiggleDir == 1 ? 1: -1;
+
+        if (this.wiggle <= -this.wiggleRange) {
+            this.wiggleDir = 1;
+        } else if (this.wiggle >= this.wiggleRange) {
+            this.wiggleDir = -1;
+        }
+    }
+
+    this.move = function() {
+        let ang = this.angle + this.wiggle;
+        var dx = Math.cos(ang * (Math.PI / 180)) * this.speed;
+        var dy = Math.sin(ang * (Math.PI / 180)) * this.speed;
         this.x += dx;
         this.y += dy;
-	}
-	this.remove = function() {
-		for (var i = 0; i < particles.length; i++) {
-			if (particles[i] == this) {
-				particles.splice(i, 1);
-			}
-		}
-	}
-	this.checkIfRemove = function() {
-		if (this.y - this.radius > canvas.height) 
-			this.y = 0;
-		else if (this.y + this.radius < 0) 
-			this.y = canvas.height;
-		if (this.x - this.radius > canvas.width) 
-			this.x = 0;
-		else if (this.x + this.radius < 0) 
-			this.x = canvas.width;
-	}
-	this.alignment = function() {
-		let sum = this.angle;
-		for (let sensed of this.sensedParticles) {
-			sum += sensed.angle;
-		}
-		this.angle = sum / (this.sensedParticles.length + 1);
-	}
-	this.changeColor = function() {
-		let r = this.r;
-		let g = this.g;
-		let b = this.b;
-		for(let p of this.sensedParticles){
-			r += p.r;
-			b += p.b;
-			g += p.g;
-		}
-		this.r = r / (this.sensedParticles.length + 1 );
-		this.b = b / (this.sensedParticles.length + 1 );
-		this.g = g / (this.sensedParticles.length + 1 );
-	}
-	this.checkSense = function() {
-		for (let particle of particles) {
-			let hyp = getHypothenuse(this, particle);
-			if (hyp < this.sensingRadius * 2) {
-				this.sensedParticles.push(particle);
-			}
-		}
-		this.alignment();
-		this.changeColor();
-	}
-	this.swing = function() {
-		if (this.isSwingRight) {
-			this.angle += 1;
-			if (this.angle > this.maxAngle) {
-				this.isSwingRight = !this.isSwingRight;
-			}
-		} else {
-			this.angle -= 1;
-			if (this.angle < this.minAngle) {
-				this.isSwingRight = !this.isSwingRight;
-			}
-		}
-	}
-	this.checkObstacle = function() {
-		for (var o of obstacles) {
-			if (getHypothenuse(this, o) < this.sensingRadius * 5) {
-				if(this.left || this.right || this.up || this.down){
-					if(this.left){
-						this.angle = 270;
-					} else if(this.right){
-						this.angle = 90;
-					} else if(this.up){
-						this.angle = 180;
-					} else if(this.down){
-						this.angle = 0;
-					}
-				} else{
-					if (this.angle >= 180 && this.angle <= 225 || this.angle >= 315 && this.angle <= 360) {
-						this.angle = 270;
-						this.left = true;
-					} else if (this.angle >= 135 && this.angle <= 180 || this.angle >= 0 && this.angle <= 45) {
-						this.angle = 90;
-						this.right = true;
-					} 
-					else if (this.angle >= 90 && this.angle <= 135 || this.angle >= 225 && this.angle <= 270) {
-						this.angle = 180;
-						this.up = true;
-					} else if (this.angle >= 45 && this.angle <= 90 || this.angle >= 270 && this.angle <= 315) {
-						this.angle = 0;
-						this.down = true;
-					}
-			}
-			} else {
-				this.up = false;
-				this.down = false;
-				this.left = false;
-				this.right = false;
-			}
-		}
-	}
-	this.update = function() {
-		this.checkIfRemove();
-		this.checkSense();
-		this.swing();
-		this.speed += this.sensedParticles.length;
-		this.sensedParticles = [];
-		this.move();
-		this.speed = this.initialSpeed;
-		return this;
-	}
-	this.draw = function() {
-		context.shadowBlur = 0;
-		context.shadowColor = 'white';
-		var my_gradient=context.createLinearGradient(0,0,0,canvas.height);
-		my_gradient.addColorStop(0,"black");
-		my_gradient.addColorStop(0.25,"white");
-		my_gradient.addColorStop(0.5,"white");
-		my_gradient.addColorStop(0.75,"white");
-		my_gradient.addColorStop(1,"black");
-		context.fillStyle = my_gradient;
-		context.beginPath();
-		context.arc(this.x,this.y,this.radius,Math.PI*2,false);
-		// context.fillStyle = "rgba("+this.r+","+this.g+","+this.b+",1)";
-		context.fillStyle = 'white';
-		context.fill();
-		context.shadowBlur = 0;
-	}
-}
+    }
 
-function Obstacle(x, y) {
-	this.x = x;
-	this.y = y;
-	this.radius = 100;
-	this.r = randomBetween(50,255);
-	this.g = randomBetween(50,255);
-	this.b = randomBetween(255,255);
-	this.checkIfRemove = function() {
-		if (this.y - this.radius > canvas.height) 
-			this.y = 0;
-		else if (this.y + this.radius < 0) 
-			this.y = canvas.height;
-		if (this.x - this.radius > canvas.width) 
-			this.x = 0;
-		else if (this.x + this.radius < 0) 
-			this.x = canvas.width;
-	}
-	this.update = function() {
-		this.x-=1;
-		this.checkIfRemove();
-		return this;
-	}
-	this.draw = function() {
-		context.beginPath();
-		context.arc(this.x, this.y, this.radius, Math.PI*2, false);
-		context.fillStyle = 'rgba(255,255,255,0.005)';
-		context.fillStyle = "rgba("+this.r+","+this.g+","+this.b+",0.005)";
-		context.fill();
-	}
+    this.update = function() {
+        this.alignment();
+        this.updateWiggle();
+        this.move();
+        return this;
+    }
+
+    this.draw = function() {
+        let ang = this.angle + this.wiggle;
+        let nosex = this.x + Math.cos(ang * (Math.PI / 180)) * 20;
+        let nosey = this.y + Math.sin(ang * (Math.PI / 180)) * 20;
+        let rwingx = this.x + Math.cos((ang + 120) * (Math.PI / 180)) * 15;
+        let rwingy = this.y + Math.sin((ang + 120) * (Math.PI / 180)) * 15;
+        let lwingx = this.x + Math.cos((ang - 120) * (Math.PI / 180)) * 15;
+        let lwingy = this.y + Math.sin((ang - 120) * (Math.PI / 180)) * 15;
+
+        context.strokeStyle = 'black';
+        context.beginPath();
+        context.moveTo(nosex, nosey);
+        context.lineTo(lwingx, lwingy);
+        context.lineTo(this.x, this.y);
+        context.lineTo(rwingx, rwingy);
+        context.lineTo(nosex, nosey);
+        context.stroke();
+
+        // context.strokeStyle = 'pink';
+        // context.beginPath();
+        // context.arc(this.x, this.y, this.innerRadius, Math.PI*2, false);
+        // context.stroke();
+        //
+        // context.strokeStyle = 'lightgreen';
+        // context.beginPath();
+        // context.arc(this.x, this.y, this.outerRadius, Math.PI*2, false);
+        // context.stroke();
+    }
 }
 
 ///////////////////////////////////////////////////////////////
 
 window.addEventListener('click', function(e) {
-	console.log(e.pageX, e.pageY);
-	for (var i = 0; i < 200; i++) {
-		particles.push(new Particle(e.pageX, e.pageY));
-	}
-	for (var i = 0; i < 100; i++) {
-		particles.shift();
-	}
+    // console.log(e.pageX, e.pageY);
 });
 
 ///////////////////////////////////////////////////////////////
